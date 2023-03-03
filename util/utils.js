a21be9282254559f5e43baf5082a3d327186ace5
Juan Delgado <juadel@hotmail.com>
2023-03-02 19:25:02 -0800

    Add files via upload
    Updating utils.js

--- util/utils.js
+++ util/utils.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import Jimp from "jimp";
-
+import axios from 'axios';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -10,18 +10,26 @@ import Jimp from "jimp";
 // RETURNS
 //    an absolute path to a filtered image locally saved file
  export async function filterImageFromURL(inputURL) {
+  
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(outpath, (img) => {
-          resolve(outpath);
+      const imageBuffer = await axios({
+          method: 'get',
+          url: inputURL, 
+          responseType: 'arraybuffer'
         });
+      const photo = await Jimp.read(imageBuffer.data);
+         
+      const outpath =
+          "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo
+          .resize(256, 256) // resize
+          .quality(60) // set JPEG quality
+          .greyscale() // set greyscale
+          .write(outpath, (img) => {
+            resolve(outpath);
+          });
+        
     } catch (error) {
       reject(error);
     }
